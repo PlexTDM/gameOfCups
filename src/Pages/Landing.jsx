b5173678252df0abd5370ad037a2962c72daad04
PlexTDM <tengisteka0631@gmail.com>
2025-01-21 12:54:02 +0800

    mini update

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -73,7 +73,7 @@ const Landing = () => {
 
                 addClassWithDelay();
             },
-            { threshold: 0.1 }
+            { threshold: 0.8 }
         );
 
         document.querySelectorAll('.animate-upDown').forEach((ref) => {
